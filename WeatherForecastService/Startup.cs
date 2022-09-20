e6a7324bb6c246e7d560629b2c00155ebf571687
Emre Erkoca <emreerkoca_related@bitbucket.org>
2022-09-20 15:08:35 +0300

    Consul integration added to Ocelot

--- WeatherForecastService/Startup.cs
+++ WeatherForecastService/Startup.cs
@@ -1,16 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using WeatherForecastService.Extensions;
 
 namespace WeatherForecastService
 {
@@ -32,10 +26,12 @@ namespace WeatherForecastService
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WeatherForecastService", Version = "v1" });
             });
+
+            services.ConfigureConsul(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IHostApplicationLifetime lifeTime)
         {
             if (env.IsDevelopment())
             {
@@ -54,6 +50,8 @@ namespace WeatherForecastService
             {
                 endpoints.MapControllers();
             });
+
+            app.RegisterWithConsul(lifeTime, Configuration);
         }
     }
 }
